6ac81c699a187837c831975ad88df79b765cc899
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-03-10 18:56:22 +0500

    feat: description section fixed

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import OwlCarousel from "react-owl-carousel";
 
 //css
 import "./Description.css";
+const mobileOptions = {
+  margin: 15,
+  loop: true,
+  responsiveClass: true,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  // navText: ["Prev", "Next"],
+  smartSpeed: 1000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+};
 
 function Description() {
   return (
@@ -16,7 +32,7 @@ function Description() {
               </div>
             </Col>
             <Col lg={5}>
-              <div className="rightText">
+              <div className="rightText pcCarousel">
                 <p>
                   Dementals are an advanced interplanetary society of degenerate
                   elemental robots existing inside a digital solar-system
@@ -43,15 +59,53 @@ function Description() {
                   and enjoy as we build Season 2 and transition The Mentalverse
                   into the 3D realm. We get the added benefit of using S1 to
                   study the economic and behavioral dynamics of our players, to
-                  optimize future iterations of the game. Season 1 is packed
-                  with features. Players can travel The System, mine resources,
-                  complete missions, create or join guilds, work for credits,
-                  steal, beg, fight, trade, raid, upgrade stats, make friends or
-                  enemies, gamble, and engage in a plethora of .DGEN activites.
-                  If you haven’t already, join our Discord and engage with other
-                  Dementals in a uniquely fun and immersive meta experience.
-                  We’ll see you there, Mental!
+                  optimize future iterations of the game.
                 </p>
+              </div>
+              <div className="rightText mobileCarousel">
+                <OwlCarousel
+                  className="owl-theme mt-2 text-center"
+                  {...mobileOptions}
+                >
+                  <p>
+                    Dementals are an advanced interplanetary society of
+                    degenerate elemental robots existing inside a digital
+                    solar-system called, The Mentalverse. The System consists of
+                    5 Planets and 1 very special Moon. Each planet contains an
+                    individually inherent mineable resource and is home to it’s
+                    own species of Dementals. You will learn more about The
+                    Mentalverse, it’s planets, their vital resources and the
+                    species that originate them as you continue to explore.
+                  </p>
+                  <p>
+                    The Dgenesis Collection of 5,000 individually unique GameFi
+                    avatars represent your citizenship and key to The
+                    Mentalverse. By holding your Demental, you will have access
+                    to play as your avatar in the current, and future Seasons of
+                    this Play-2-Earn meta.
+                  </p>
+
+                  <p>
+                    Each Season represents an evolutionary milestone for the
+                    project. The Mentalverse | Season 1 is our proof-of-concept
+                    and is playable NOW! We’ve managed to develop the first-ever
+                    P2E metaRPG gamified Discord server for our community to
+                    play and enjoy as we build Season 2 and transition The
+                    Mentalverse into the 3D realm. We get the added benefit of
+                    using S1 to study the economic and behavioral dynamics of
+                    our players, to optimize future iterations of the game.
+                  </p>
+                  <p>
+                    Season 1 is packed with features. Players can travel The
+                    System, mine resources, complete missions, create or join
+                    guilds, work for credits, steal, beg, fight, trade, raid,
+                    upgrade stats, make friends or enemies, gamble, and engage
+                    in a plethora of .DGEN activites. If you haven’t already,
+                    join our Discord and engage with other Dementals in a
+                    uniquely fun and immersive meta experience. We’ll see you
+                    there, Mental!
+                  </p>
+                </OwlCarousel>
               </div>
             </Col>
           </Row>
